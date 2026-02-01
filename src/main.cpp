5848cea65c24056010333c1381529dd1160bdaed
thejohnmathews <thejohnmathews@gmail.com>
2026-01-31 23:58:47 -0500

    Hello World

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+// main.cpp - main file for metro-de-madrid-CLI application
+
+// include statements
+#include <iostream>
+#include <bits/stdc++.h>
+
+// global variables
+
+// main function
+int main(void) {
+
+    std::cout << "Buenas del metro de madrid cli!" << std::endl;
+    return 0;
+}
